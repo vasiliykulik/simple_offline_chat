86c3305ba96e2dc067ee0494e9445c229af7ce0b
Vasiliy Kylik <detroit.vv@gmail.com>
2017-02-16 20:39:21 +0200

    Message Service in UserDetailServiceImplTest

--- src/test/java/ua/goit/offline/chat/security/UserDetailsServiceImplTest.java
+++ src/test/java/ua/goit/offline/chat/security/UserDetailsServiceImplTest.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import ua.goit.offline.chat.dao.MessagesService;
 import ua.goit.offline.chat.dao.UserService;
 import ua.goit.offline.chat.entity.User;
 
@@ -18,11 +19,13 @@ import static org.junit.Assert.assertNotNull;
 public class UserDetailsServiceImplTest {
     private FileSystemXmlApplicationContext applicationContext;
     private UserService userService;
+    private MessagesService messagesService;
 
     @Before
     public void setUp() throws Exception {
         applicationContext = new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/database-context.xml");
         userService = applicationContext.getBean(UserService.class);
+        messagesService = applicationContext.getBean(MessagesService.class);
     }
 
     @After
@@ -34,6 +37,11 @@ public class UserDetailsServiceImplTest {
     public void loadUserByUsername() throws Exception {
         User user = userService.getUser("test");
         assertNotNull(user);
+    }
+
+    @Test
+    public void getAllMessages() throws Exception {
+        messagesService.getAllMessages();
     }
 
 }
